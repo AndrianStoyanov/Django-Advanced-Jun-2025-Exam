8ddb267d7bcbd1d7f2249fce33a28614e846dc3d
AndrianStoyanov <ancast@abv.bg>
2025-08-07 14:49:14 +0300

    feat: I repeare bug

--- ConstructionProjectManagement/ConstructionProjectManagement/urls.py
+++ ConstructionProjectManagement/ConstructionProjectManagement/urls.py
@@ -20,9 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('common/', include('common.urls')),
+    path('', include('common.urls')),
     path('projects/', include('projects.urls')),
 
     path('builders/', include('builders.urls')),
--- ConstructionProjectManagement/common/urls.py
+++ ConstructionProjectManagement/common/urls.py
@@ -3,6 +3,6 @@ from django.urls.conf import path
 from common import views
 
 urlpatterns = [
-    path('home/', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('home-ex/', views.HomeExView.as_view(), name='home-ex'),
 ]
